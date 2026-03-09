684cd276103647695da3b574c55b8e0a4f00bb68
Administrator <Administrator@DESKTOP-VSNCEKE>
2026-03-10 07:25:48 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1// 命名空间
+{
+    internal class Program // 类名
+    {
+        /// <summary>
+        /// main方法，程序入口
+        /// </summary>
+        /// <param name="args">参数</param>
+        static void Main(string[] args)// 
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ConsoleAppFW/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;// 引用的库
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFW//命名空间
+{
+    /// <summary>
+    /// 类名
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// 程序入口
+        /// </summary>
+        /// <param name="args">字符串数组</param>
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
