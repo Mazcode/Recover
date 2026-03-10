e4d4d20f89be441aea0364187bf67e8109e563e6
Mazcode <maxliljh@163.com>
2026-03-10 08:51:09 +0800

    创建复习数据类型的类datatype

---
+++ ConsoleAppFW/datatype.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFW
+{
+    /// <summary>
+    /// 复习数据类型
+    /// </summary>
+    internal class datatype
+    {
+    }
+}
